9a60cd8f921f3f9ddea1221187984d458b131f5d
renatoalves0 <renatoalves97.123@gmail.com>
2020-12-15 15:22:16 -0300

    Substituindo Cliente por Servidor

--- index.js
+++ index.js
@@ -1,50 +1,11 @@
 const coap = require('coap')
+const server = coap.createServer()
 
-let liga_desliga = '0'
+server.on('request', (req, res) => {
+    console.log(req.url.split('/')[1]);
+    console.log(Buffer.from(req.payload).toString() + '\n');
+    res.end('0')
+})
 
-post = () => {
-    let msg = coap.request({
-        host: '192.168.0.4',
-        pathname: 'led',
-        method: 'POST',
-        // confirmable: true,
-    })
-
-    msg.write(liga_desliga)
-
-    if (liga_desliga == '0') liga_desliga = '1'
-    else liga_desliga = '0'
-
-    msg.on('response', function (res) {
-        console.log('\n');
-        res.pipe(process.stdout)
-        // res.on('end', function () {
-        //     process.exit(0)
-        // })
-    })
-
-    msg.end()
-}
-
-get = () => {
-
-    let req = coap.request({
-        host: '192.168.0.4',
-        pathname: 'led',
-        // observe: true
-    })
-
-    req.on('response', function (res) {
-        console.log('\n')
-        res.pipe(process.stdout)
-    })
-    req.end()
-}
-
-setInterval(() => {
-
-    get()
-
-    post()
-
-}, 1000)
+server.listen(() => {
+})
