1c76644e17e8ac1a6514c79c5867906b766b4f24
renatoalves0 <renatoalves97.123@gmail.com>
2021-01-11 17:25:42 -0300

    criando um arquivo Servidor separado

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const coap = require('coap')
-const server = coap.createServer()
-
-server.on('request', (req, res) => {
-    console.log(req.url.split('/')[1]);
-    console.log(Buffer.from(req.payload).toString() + '\n');
-    res.end('0')
-})
-
-server.listen(() => {
-})
+var servidor = require('./servidor')
---
+++ servidor.js
@@ -0,0 +1,12 @@
+const coap = require('coap')
+const server = coap.createServer()
+
+server.on('request', (req, res) => {
+    console.log(req.url.split('/')[1])
+    console.log(Buffer.from(req.payload).toString() + '\n')
+    // res.end('0')
+})
+
+server.listen(() => {
+    console.log('Servidor COAP Online')
+})
