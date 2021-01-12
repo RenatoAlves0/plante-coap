75cc0e44865c37fdc101f61182d6ecd2af688629
renatoalves0 <renatoalves97.123@gmail.com>
2021-01-12 14:39:37 -0300

    Finalizando modificações para experimento de IoT

--- servidor.js
+++ servidor.js
@@ -5,23 +5,27 @@ const client_ntp = new NTP("a.st1.ntp.br", 123, { timeout: 5000 })
 const jsonexport = require('jsonexport')
 fs = require('fs')
 let dados = { chegada: undefined, delay_ms: undefined, jitter_ms: undefined }
-let delay_anterior = 0, array_dados = [], arquivo
+let delay_anterior = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+let array_dados = [[], [], [], [], [], [], [], [], [], []]
 
 server.on('request', (req, res) => {
-    // console.log(req.url.split('/')[1])
+    let url = req.url.split('/')[1]
+    console.log('Url: ' + url)
+    console.log('Tamanho Url: ' + Buffer.byteLength(url) + ' bytes')
+    console.log('Tamanho Mensagem: ' + Buffer.byteLength(req.payload) + ' bytes')
     dados = JSON.parse(Buffer.from(req.payload).toString())
     dados.envio = data_saida(dados.envio_s, dados.envio_us)
 
     client_ntp
         .syncTime()
-        .then((time) => {
-            dados.chegada = time.transmitTimestamp.toString().split('.')[0]
-            let envio = new Date(parseInt(dados.envio))
-            let chegada = new Date(parseInt(dados.chegada))
-            dados.delay_ms = Math.abs(chegada - envio)
-            dados.jitter_ms = Math.abs(delay_anterior - dados.delay_ms)
-            delay_anterior = dados.delay_ms
-            dadosToFile(dados)
+        .then(async (time) => {
+            dados.chegada = await time.transmitTimestamp.toString().split('.')[0]
+            let envio = await new Date(parseInt(dados.envio))
+            let chegada = await new Date(parseInt(dados.chegada))
+            dados.delay_ms = await Math.abs(chegada - envio)
+            dados.jitter_ms = await Math.abs(delay_anterior[parseInt(url)] - dados.delay_ms)
+            delay_anterior[parseInt(url)] = await dados.delay_ms
+            await dadosToFile(dados, parseInt(url))
         })
 
     // res.end('0')
@@ -37,19 +41,20 @@ data_saida = (envio_s, envio_us) => {
     let qtd_zero = 6 - envio_us.length
     let qtd_ms = 3 - qtd_zero
     for (let i = 0; i < qtd_zero; i++)
-        envio_s += "0"
+        envio_s += '0'
     envio_s += envio_us.slice(0, qtd_ms)
     return envio_s
 }
 
-dadosToFile = (dados) => {
-    array_dados.push(dados)
+dadosToFile = async (dados, i) => {
+    console.log('i ===> ' + i)
     console.log(dados)
-    if (dados.id == 24) {
-        jsonexport(array_dados, (err, csv) => {
+    await array_dados[i].push(dados)
+    if (dados.id == 15) {
+        await jsonexport(array_dados[i], async (err, csv) => {
             if (err) return console.error(err)
-            arquivo = csv
-            fs.writeFile('a.csv', arquivo, (err) => {
+            let arquivo = await csv
+            await fs.writeFile('a_' + i + '.csv', arquivo, async (err) => {
                 if (err) return console.log(err)
             })
         })
