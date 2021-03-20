635c246e9e5be21ff1949b0d54275a630cd436ac
renatoalves0 <renatoalves97.123@gmail.com>
2021-03-20 15:34:14 -0300

    NOVO EXPERIMENTO

--- servidor.js
+++ servidor.js
@@ -1,36 +1,38 @@
 const coap = require('coap')
 const server = coap.createServer()
-const NTP = require("ntp-time").Client
-const client_ntp = new NTP("a.st1.ntp.br", 123, { timeout: 5000 })
 const jsonexport = require('jsonexport')
 fs = require('fs')
 let dados = { chegada: undefined, delay_ms: undefined, jitter_ms: undefined }
-let delay_anterior = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
-let gravado = [false, false, false, false, false, false, false, false, false, false]
-let array_dados = [[], [], [], [], [], [], [], [], [], []]
+let delay_anterior = -1
+let gravado = false
+let array_dados = []
 
 server.on('request', (req, res) => {
     let url = req.url.split('/')[1]
     console.log('Url: ' + url)
-    // console.log('Tamanho Url: ' + Buffer.byteLength(url) + ' bytes')
-    // console.log('Tamanho Mensagem: ' + Buffer.byteLength(req.payload) + ' bytes')
+    console.log('Tamanho Url: ' + Buffer.byteLength(url) + ' bytes')
+    console.log('Tamanho Mensagem: ' + Buffer.byteLength(req.payload) + ' bytes')
     dados = JSON.parse(Buffer.from(req.payload).toString())
-    dados.envio = data_saida(dados.envio_s, dados.envio_us)
-
-    client_ntp
-        .syncTime()
-        .then(async (time) => {
-            dados.chegada = await time.transmitTimestamp.toString().split('.')[0]
-            let envio = await new Date(parseInt(dados.envio))
-            let chegada = await new Date(parseInt(dados.chegada))
-            dados.delay_ms = await Math.abs(chegada - envio)
-            if (delay_anterior[parseInt(url)] == -1)
-                dados.jitter_ms = 0
-            else
-                dados.jitter_ms = await Math.abs(delay_anterior[parseInt(url)] - dados.delay_ms)
-            delay_anterior[parseInt(url)] = await dados.delay_ms
-            await dadosToFile(dados, parseInt(url))
-        })
+    if (dados.fim) {
+        gravar()
+        console.log(dados)
+    }
+    else {
+        dados.chegada = String(new Date().getTime() / 1000)
+        dados.chegada = data_format_ms(dados.chegada.split('.')[0], dados.chegada.split('.')[1])
+        dados.envio = data_format_us(dados.s, dados.us)
+        dados.envio = data_format_us(dados.s, dados.us)
+        let envio = new Date(parseInt(dados.envio))
+        let chegada = new Date(parseInt(dados.chegada))
+        dados.delay_ms = Math.abs(chegada - envio)
+        if (delay_anterior == -1)
+            dados.jitter_ms = 0
+        else
+            dados.jitter_ms = Math.abs(delay_anterior - dados.delay_ms)
+        delay_anterior = dados.delay_ms
+        console.log(dados)
+        array_dados.push(dados)
+    }
 
     // res.end('0')
 })
@@ -39,29 +41,35 @@ server.listen(() => {
     console.log('Servidor COAP Online')
 })
 
-data_saida = (envio_s, envio_us) => {
-    envio_s = envio_s.toString()
-    envio_us = envio_us.toString()
-    let qtd_zero = 6 - envio_us.length
-    let qtd_ms = 3 - qtd_zero
+data_format_ms = (s, us) => {
+    s = s.toString()
+    us = us.toString()
+    let qtd_zero = 3 - us.length
     for (let i = 0; i < qtd_zero; i++)
-        envio_s += '0'
-    envio_s += envio_us.slice(0, qtd_ms)
-    return envio_s
+        s += '0'
+    let qtd_ms = 3 - qtd_zero
+    s += us.slice(0, qtd_ms)
+    return s
 }
 
-dadosToFile = async (dados, i) => {
-    // console.log('i ===> ' + i)
-    // console.log(dados)
-    await array_dados[i].push(dados)
-    if (dados.id >= 14 && !gravado[i]) {
-        await jsonexport(array_dados[i], async (err, csv) => {
-            if (err) return console.error(err)
-            let arquivo = await csv
-            await fs.writeFile('f16_' + i + '.csv', arquivo, async (err) => {
-                if (err) return console.log(err)
-                else gravado[i] = true
-            })
+data_format_us = (s, us) => {
+    s = s.toString()
+    us = us.toString()
+    let qtd_zero = 6 - us.length
+    for (let i = 0; i < qtd_zero; i++)
+        s += '0'
+    let qtd_ms = 3 - qtd_zero
+    s += us.slice(0, qtd_ms)
+    return s
+}
+
+gravar = () => {
+    jsonexport(array_dados, (err, csv) => {
+        if (err) return console.error(err)
+        let arquivo = csv
+        fs.writeFile('exp2/1.csv', arquivo, (err) => {
+            if (err) return console.log(err)
+            else gravado = true
         })
-    }
+    })
 }
