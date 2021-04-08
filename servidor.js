e55d595537a450166eb601d0b24ca2e74bcee9d6
renatoalves0 <renatoalves97.123@gmail.com>
2021-04-08 09:29:41 -0300

    retirando experimentos anteriores

--- servidor.js
+++ servidor.js
@@ -1,41 +1,48 @@
 const coap = require('coap')
 const server = coap.createServer()
 const jsonexport = require('jsonexport')
-fs = require('fs')
+const fs = require('fs')
 let dados = { chegada: undefined, delay_ms: undefined, jitter_ms: undefined }
-let delay_anterior = -1
-let gravado = false
-let array_dados = []
+let array_dados = [], delay_anterior = -1, ms = 0, mi = 0, nome_arquivo
 
 server.on('request', (req, res) => {
-    let url = req.url.split('/')[1]
-    console.log('Url: ' + url)
-    console.log('Tamanho Url: ' + Buffer.byteLength(url) + ' bytes')
-    console.log('Tamanho Mensagem: ' + Buffer.byteLength(req.payload) + ' bytes')
-    dados = JSON.parse(String(Buffer.from(req.payload)))
-    if (dados.fim) {
-        gravar()
+    dados = JSON.parse(String(req.payload))
+    if (dados.reset) {
+        mi = 0
+        ms = 0
+        array_dados = []
+        delay_anterior = -1
+        nome_arquivo = 'exp1/' + dados.nome_arquivo
+        console.log('RESETANDO\n')
         console.log(dados)
-    }
-    else {
-        dados.chegada = String(new Date().getTime() / 1000)
-        dados.chegada = data_format_ms(dados.chegada.split('.')[0], dados.chegada.split('.')[1])
-        dados.envio = data_format_us(dados.s, dados.us)
-        let envio = new Date(parseInt(dados.envio))
-        let chegada = new Date(parseInt(dados.chegada))
-        dados.delay_ms = chegada - envio
-        if (dados.delay_ms < 0) dados.delay_ms = 0
+    } else
+        if (dados.fim) {
+            gravar(dados.total)
+            console.log(dados)
+        }
+        else {
+            dados.chegada = String(new Date().getTime() / 1000)
+            dados.chegada = data_format_ms(dados.chegada.split('.')[0], dados.chegada.split('.')[1])
+            dados.envio = data_format_us(dados.s, dados.us)
+            let envio = new Date(parseInt(dados.envio))
+            let chegada = new Date(parseInt(dados.chegada))
+            dados.delay_ms = chegada - envio
+            if (dados.delay_ms < 0) dados.delay_ms = 0
 
-        if (delay_anterior == -1)
-            dados.jitter_ms = 0
-        else
-            dados.jitter_ms = Math.abs(delay_anterior - dados.delay_ms)
-        delay_anterior = dados.delay_ms
-        console.log(dados)
-        array_dados.push(dados)
-    }
-
-    // res.end('0')
+            if (delay_anterior == -1)
+                dados.jitter_ms = 0
+            else
+                dados.jitter_ms = Math.abs(delay_anterior - dados.delay_ms)
+            delay_anterior = dados.delay_ms
+            delete dados.a
+            mi++
+            ms = ms + dados.delay_ms
+            console.log('Mensagem: ' + Buffer.byteLength(req.payload) + ' B')
+            console.log('Qtd Msg: ' + mi)
+            console.log('Delay::::::::::::::: ' + dados.delay_ms + ' ms')
+            console.log('Média: ' + (ms / mi).toFixed(2) + ' ms\n')
+            array_dados.push(dados)
+        }
 })
 
 server.listen(() => {
@@ -43,34 +50,37 @@ server.listen(() => {
 })
 
 data_format_ms = (s, us) => {
-    s = String(s)
-    us = String(us) == 'undefined' ? '000' : String(us)
+    s = String(s) || '0'
+    us = String(us).toLowerCase().indexOf('undefined') > -1 ? '000' : String(us)
     let qtd_zero = 3 - us.length
     for (let i = 0; i < qtd_zero; i++)
-        s += '0'
-    let qtd_ms = 3 - qtd_zero
-    s += us.slice(0, qtd_ms)
+        us += '0'
+    s += us.slice(0, 3)
     return s
 }
 
 data_format_us = (s, us) => {
     s = String(s)
-    us = String(us) == 'undefined' ? '000' : String(us)
+    us = String(us).toLowerCase().indexOf('undefined') > -1 ? '000' : String(us)
     let qtd_zero = 6 - us.length
     for (let i = 0; i < qtd_zero; i++)
-        s += '0'
-    let qtd_ms = 3 - qtd_zero
-    s += us.slice(0, qtd_ms)
+        us += '0'
+    s += us.slice(0, 3)
     return s
 }
 
-gravar = () => {
+gravar = (total) => {
+    let relatorio = nome_arquivo + ': ' + mi + '/' + total + ', ' + (ms / mi).toFixed(2) + ' ms\n'
+    fs.appendFile('relatorio_exp1.txt', relatorio, (err) => {
+        if (err) throw err
+        console.log('Updated!')
+    })
+
     jsonexport(array_dados, (err, csv) => {
         if (err) return console.error(err)
         let arquivo = csv
-        fs.writeFile('exp2/C0_3.csv', arquivo, (err) => {
+        fs.writeFile(nome_arquivo, arquivo, (err) => {
             if (err) return console.log(err)
-            else gravado = true
         })
     })
 }
